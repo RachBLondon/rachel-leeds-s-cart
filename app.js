69bd927fedc6d1a83ef5cf4aec8820bd8c3abd67
Rachel Black <rachel.black1984@googlemail.com>
2016-01-09 19:15:56 +0000

    basic mongodb setup, returning three user tupes http://localhost:3000/users/userlist

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/cart-rb');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +25,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
 
 app.use('/', routes);
 app.use('/users', users);
@@ -58,3 +69,7 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
+
+// http://stackoverflow.com/questions/28651028/cannot-find-module-build-release-bson-code-module-not-found-js-bson
