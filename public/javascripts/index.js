e2db5fc294af4588150cdca4a19a3beb96d293a5
Rachel Black <rachel.black1984@googlemail.com>
2016-01-10 12:50:27 +0000

    tv channels obj and logic created in frontend js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,32 @@
 (function(){
   console.log("hello world")
   // document.cookie"userId=0003";
-  document.cookie="userId=0004";
+  document.cookie="userId=0003";
   var out = new XMLHttpRequest()
+
+  var sportsChannels = {};
+  var newsChanels = {
+    newsOptionOne : "Sky News",
+    newsOptionTwo : "Sky Sports News"
+  };
+
+  var findLocationSports = function(location){
+    if (location === "London"){
+      sportsChannels.sportsOptionOne = "Arsenal TV";
+      sportsChannels.sportsOptionTwo = "Chelsea TV"
+    } else if ( location = "Liverpool"){
+      sportsChannels.sportsOptionOne = "Liverpool TV"
+    }
+    return sportsChannels;
+  }
 
   out.onreadystatechange = function() {
     if (out.readyState === 4 && out.status === 200) {
-      //handle callback here
+      //handle callback her
       var location = JSON.parse(out.responseText)[0].location;
       console.log(location);
+      console.log(findLocationSports(location));
+      console.log(newsChanels);
       }
   }
   console.log("sending XML");
